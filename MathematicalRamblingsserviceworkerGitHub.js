79f7d6e50448c9c80aaa0e6b6e398414f879369f
antoniovandre <a.vandre.g@gmail.com>
2020-03-03 22:14:32 -0300

    Testando service worker.

---
+++ MathematicalRamblingsserviceworkerGitHub.js
@@ -0,0 +1,46 @@
+// Projeto Mathematical Ramblings (mathematicalramblings.blogspot.com).
+
+// Arquivo service worker para "https://antoniovandre.github.io/MathematicalRamblingsjs/".
+
+// Última atualização: 03-03-2020.
+
+var CACHE_NAME = 'Mathematical-Ramblings-cache-v1';
+
+var urlsToCache = [
+  'MathematicalRamblings.js',
+];
+
+self.addEventListener('install', function (event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache){return cache.addAll(urlsToCache);})
+  );
+});
+
+self.addEventListener('activate', function (event) {
+  var cacheWhitelist = ['Mathematical-Ramblings-cache-v1'];
+  event.waitUntil(
+    caches.keys().then(function (cacheNames) {
+      return Promise.all(cacheNames.map(function (cacheName) {
+        if (cacheWhitelist.indexOf(cacheName) === -1) {
+          return caches.delete(cacheName);
+        }
+      })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+    }
+    )
+  );
+});
+
