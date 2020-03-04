2c98bdcf27943a9e1ecde088e9327c998803097e
antoniovandre <a.vandre.g@gmail.com>
2020-03-04 09:21:06 -0300

    Testando service worker.

--- MathematicalRamblingsserviceworkerGitHub.js
+++ MathematicalRamblingsserviceworkerGitHub.js
@@ -1,71 +1,52 @@
-/*
- Copyright 2016 Google Inc. All Rights Reserved.
- Licensed under the Apache License, Version 2.0 (the "License");
- you may not use this file except in compliance with the License.
- You may obtain a copy of the License at
- http://www.apache.org/licenses/LICENSE-2.0
- Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS,
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- See the License for the specific language governing permissions and
- limitations under the License.
- */
+// Projeto Mathematical Ramblings (mathematicalramblings.blogspot.com).
 
-// Names of the two caches used in this version of the service worker.
-// Change to v2, etc. when you update any of the local resources, which will
-// in turn trigger the install event again.
-const PRECACHE = 'precache-v1';
-const RUNTIME = 'runtime';
+// Arquivo service worker para "https://antoniovandre.github.io/MathematicalRamblingsjs/".
 
-// A list of local resources we always want to be cached.
-const PRECACHE_URLS = [
-  'MathematicalRamblings.js'
+// Última atualização: 03-03-2020.
+
+var CACHE_NAME = 'Mathematical-Ramblings-cache-v1';
+
+var urlsToCache = [
+  'MathematicalRamblings.js',
 ];
 
-// The install handler takes care of precaching the resources we always need.
-self.addEventListener('install', event => {
+self.importScripts('MathematicalRamblings.js');
+
+self.addEventListener('install', function (event) {
   event.waitUntil(
-    caches.open(PRECACHE)
-      .then(cache => cache.addAll(PRECACHE_URLS))
-      .then(self.skipWaiting())
+    caches.open(CACHE_NAME)
+      .then(function(cache){return cache.addAll(urlsToCache);})
   );
 });
 
-// The activate handler takes care of cleaning up old caches.
-self.addEventListener('activate', event => {
-  const currentCaches = [PRECACHE, RUNTIME];
+self.addEventListener('activate', function (event) {
+  var cacheWhitelist = ['Mathematical-Ramblings-cache-v1'];
   event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return cacheNames.filter(cacheName => !currentCaches.includes(cacheName));
-    }).then(cachesToDelete => {
-      return Promise.all(cachesToDelete.map(cacheToDelete => {
-        return caches.delete(cacheToDelete);
-      }));
-    }).then(() => self.clients.claim())
-  );
-});
-
-// The fetch handler serves responses for same-origin resources from a cache.
-// If no response is found, it populates the runtime cache with the response
-// from the network before returning it to the page.
-self.addEventListener('fetch', event => {
-  // Skip cross-origin requests, like those for Google Analytics.
-  if (event.request.url.startsWith(self.location.origin)) {
-    event.respondWith(
-      caches.match(event.request).then(cachedResponse => {
-        if (cachedResponse) {
-          return cachedResponse;
+    caches.keys().then(function (cacheNames) {
+      return Promise.all(cacheNames.map(function (cacheName) {
+        if (cacheWhitelist.indexOf(cacheName) === -1) {
+          return caches.delete(cacheName);
         }
-
-        return caches.open(RUNTIME).then(cache => {
-          return fetch(event.request).then(response => {
-            // Put a copy of the response in the runtime cache.
-            return cache.put(event.request, response.clone()).then(() => {
-              return response;
-            });
-          });
-        });
       })
-    );
-  }
+      );
+    })
+  );
 });
+
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+    }
+    )
+  );
+});
+
+self.addEventListener('message', function (event) {
+  event.data;
+});
+
