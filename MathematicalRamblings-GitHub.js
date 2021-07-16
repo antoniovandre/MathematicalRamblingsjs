9c613ab50d55b742ac9104fe10dabbd213bda17c
Antonio Vandré Pedrosa Furtunato Gomes <a.vandre.g@gmail.com>
2021-07-15 22:17:27 -0300

    Um pequeno ajuste de output.

--- MathematicalRamblings-GitHub.js
+++ MathematicalRamblings-GitHub.js
@@ -6,7 +6,7 @@
 
 // Sugestão ou comunicar erro: "a.vandre.g@gmail.com".
 
-// Última atualização: 14-07-2021.
+// Última atualização: 15-07-2021.
 
 // Início escopo desenvolvido por Antonio Vandré Pedrosa Furtunato Gomes (bit.ly/antoniovandre_legadoontologico).
 
@@ -16,7 +16,7 @@ console.log("                                                  \n          .\',;
 
 // Versão do MathematicalRamblings.js.
 
-function antoniovandremathematicalramblingsjsversao(){return "14-07-2021";}
+function antoniovandremathematicalramblingsjsversao(){return "15-07-2021";}
 
 // Fim mensagem de inicialização no console.log.
 
@@ -7095,7 +7095,7 @@ function antoniovandremediaantoniovandre(str, avisoanexo)
 
 // Matriz alfanumerica.
 
-var antoniovandrematrizalfanumerica = [["0",0],["1",1],["2",2],["3",3],["4",4],["5",5],["6",6],["7",7],["8",8],["9",9],["a",10],["b",11],["c",12],["d",13],["e",14],["f",15],["g",16],["h",17],["i",18],["j", 19],["k",20],["l",21],["m",22],["n",23],["o",24],["p",25],["q",26],["r",27],["s",28],["t",29],["u",30],["v",31],["w",32],["x",33],["y",34],["z",35]]
+var antoniovandrematrizalfanumerica = [["0",0],["1",1],["2",2],["3",3],["4",4],["5",5],["6",6],["7",7],["8",8],["9",9],["a",10],["b",11],["c",12],["d",13],["e",14],["f",15],["g",16],["h",17],["i",18],["j", 19],["k",20],["l",21],["m",22],["n",23],["o",24],["p",25],["q",26],["r",27],["s",28],["t",29],["u",30],["v",31],["w",32],["x",33],["y",34],["z",35]];
 
 // Retorna a posição de um termo na matriz alfanumérica. Retorna a string "e" caso um erro genérico ocorra.
 
@@ -7117,7 +7117,7 @@ function antoniovandrematrizalfanumericatermo(i)
 
 	if (antoniovandrenumeronatural (i.toString()) == "e") return "e";
 
-	if (isnan(antoniovandrematrizalfanumerica[i][0]))
+	if (isNaN(antoniovandrematrizalfanumerica[i][0]))
 		return "e_"
 	else
 		return antoniovandrematrizalfanumerica[i][0];
@@ -7330,7 +7330,7 @@ function antoniovandrefuncaomaisproxima(str, avisoanexo)
 							if (antoniovandrecompararstrings(typeof resultpart, "string") == "e")
 								return "e"
 							else
-								resultpart = Math.abs(resultpart - ordenada);
+								return resultpart;
 							}
 						}
 					}
